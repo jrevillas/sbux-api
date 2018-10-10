290c5ee2ee85687af6d4a1c1f58d55931303e81b
Javier Revillas <j.revillas@payvision.com>
2018-10-10 15:35:24 +0200

    use body parser in orders handler

--- orders.js
+++ orders.js
@@ -1,6 +1,15 @@
 'use strict';
 
+const parseBody = require('./body-parser');
+
 module.exports.handler = async (event, context) => {
+  const parsedBody = parseBody(event.body);
+  if (parsedBody === undefined) {
+    return {
+      body: '',
+      statusCode: 400
+    };
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
