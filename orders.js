09203a7756d75cce9e868000427f08bcbdbbe3cd
Javier Revillas <j.revillas@payvision.com>
2018-10-14 17:29:11 +0200

    remove comments from main handler

--- orders.js
+++ orders.js
@@ -17,7 +17,4 @@ module.exports.handler = async (event, context) => {
       input: event,
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
