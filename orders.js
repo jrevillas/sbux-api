bf8f76a177e434ea17f6a165b089ae910c446191
Javier Revillas <j.revillas@payvision.com>
2018-10-21 20:31:24 +0200

    enable random mode via query string parameters

--- orders.js
+++ orders.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const parseBody = require('./body-parser');
+const { isRandomMode, randomOrder } = require('./utils');
 const validateOrder = require('./validator');
 
 module.exports.handler = async (event, context) => {
+  if (isRandomMode(event.queryStringParameters)) {
+    event.body = randomOrder();
+  }
   const parsedBody = parseBody(event.body);
   if (parsedBody === undefined) {
     return {
--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
 const menuDescriptor = require('./menu.json');
 
 module.exports = {
+  isRandomMode: queryStringParameters => {
+    if (queryStringParameters === null) { return false; }
+    return queryStringParameters.random === 'true';
+  },
   randomOrder: () => {
     const order = menuDescriptor[Math.floor(Math.random() * menuDescriptor.length)];
     return JSON.stringify(Object.assign({}, order, {price: undefined}));
