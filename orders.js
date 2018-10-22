6b7da5fdccefb58a1052ea94c0ad07ff7e4f7544
Javier Revillas <j.revillas@payvision.com>
2018-10-22 21:58:00 +0200

    WIP: use DynamoDB to persist orders

--- orders.js
+++ orders.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const aws = require('aws-sdk');
 const parseBody = require('./body-parser');
 const { isRandomMode, randomOrder } = require('./utils');
+const uuid = require('uuid/v4');
 const validateOrder = require('./validator');
+
+const dynamoDb = new aws.DynamoDB({
+  region: 'eu-west-1'
+});
 
 module.exports.handler = async (event, context) => {
   if (isRandomMode(event.queryStringParameters)) {
@@ -22,6 +28,18 @@ module.exports.handler = async (event, context) => {
       statusCode: 400
     };
   }
+  const dynamoDbItem = {
+    Item: {
+      OrderId: {S: uuid()},
+      OrderInformation: {M: {}},
+      ShopId: {N: '0'}
+    },
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: 'StarbucksOrders'
+  };
+  await dynamoDb.putItem(dynamoDbItem).promise()
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
   return {
     body: JSON.stringify(order),
     statusCode: 200
