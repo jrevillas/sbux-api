4baacb869f82e794d5630d629b2e5a132dffc5c6
Javier Revillas <j.revillas@payvision.com>
2018-10-28 15:20:04 +0100

    retrieve Payvision credentials from environment

---
+++ payments.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+const pvGatewayEndpoint = process.env.PAYVISION_GATEWAY_ENDPOINT;
+const pvMemberGuid = process.env.PAYVISION_MEMBER_GUID;
+const pvMemberId = process.env.PAYVISION_MEMBER_ID;
