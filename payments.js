3325214df3ce91e6e1969e89b200b4931ba67729
Javier Revillas <j.revillas@payvision.com>
2018-10-28 20:24:00 +0100

    complete integration with payment processor

--- payments.js
+++ payments.js
@@ -6,8 +6,32 @@ const pvMemberId = process.env.PAYVISION_MEMBER_ID;
 
 const processPayment = async paymentInformation => {
   try {
-    return '8MA5N';
-  } catch(err) {}
+    const res = await axios.post(pvGatewayEndpoint, {
+      amount: '100',
+      avsAddress: '',
+      avsZip: '',
+      cardExpiryMonth: '',
+      cardExpiryYear: '',
+      cardholder: '',
+      cardNumber: '',
+      cardCvv: '',
+      countryId: '724', // Spain
+      currencyId: '978', // EUR
+      dbaCity: '',
+      dbaName: '',
+      memberGuid: pvMemberGuid,
+      memberId: pvMemberId,
+      merchantAccountType: '1',
+      trackingMemberCode: ''
+    });
+    if (res.data.authorization_code !== undefined) {
+      return res.data.authorization_code;
+    }
+    throw new Error();
+  } catch(err) {
+    console.log('error dialing remote payment gateway');
+    throw new Error();
+  }
 }
 
 module.exports = processPayment;
